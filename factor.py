0aaa2ff7cc1f412dde47245e1f864839cfdae2ba
Nathan Smith <nathansmithrules@gmail.com>
2018-08-06 09:22:21 -0500

    Initial commit

---
+++ factor.py
@@ -0,0 +1,9 @@
+# this porgram is supposed to be able to factor a polynomial
+# x^2+4x+4 will be the first test
+
+# take coefficients from user
+coeX = input('What is the coefficient for power x?')
+coe1 = input('What is the coefficient for power 1?')
+
+def factor():
+    
