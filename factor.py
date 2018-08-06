c811c0cb9603eb7d365d56d2923fe54f54d73842
Nathan Smith <nathansmithrules@gmail.com>
2018-08-06 10:06:23 -0500

    First Version
    Really basic program. Can currently do polynomials up to the power of 2 with no leading coefficient. zeros can't be outside of range -50 to 50.

--- factor.py
+++ factor.py
@@ -1,9 +1,27 @@
 # this porgram is supposed to be able to factor a polynomial
-# x^2+4x+4 will be the first test
+# x**2+4x-12 will be the first test, answer is (x-2)(x+6)
+# also can't go higher than x**2 or do equations with coefficients for x**2
 
 # take coefficients from user
-coeX = input('What is the coefficient for power x?')
-coe1 = input('What is the coefficient for power 1?')
+coeX = int(input('What is the coefficient for power x?'))
+coe1 = int(input('What is the coefficient for power 1?'))
 
-def factor():
-    
+# coe1 is the higher power, coe2 is the lower power
+
+
+# finds the zeros of the equation
+for i in range(-50, 50):
+    if i**2 + coeX*i + coe1 == 0:
+        zero1 = i
+        break
+for i in range(zero1+1, 50):
+    if i**2 + coeX*i + coe1 == 0:
+        zero2 = i
+        break
+
+
+def answer():
+    print('(x + ' + str(zero1) + ')(x + ' + str(zero2) + ')')
+
+
+answer()
