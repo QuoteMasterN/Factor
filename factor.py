985ebb3bbd38c629f5504f3b6bf23d69cc52784d
Nathan Smith <nathansmithrules@gmail.com>
2018-08-08 12:24:39 -0500

    Squares
    Added a workaround for polynomials where there's only one zero. Also fixed a mixup involving the positive or negative of the anwser.

--- factor.py
+++ factor.py
@@ -1,4 +1,4 @@
-# this porgram is supposed to be able to factor a polynomial
+# this program is supposed to be able to factor a polynomial
 # x**2+4x-12 will be the first test, answer is (x-2)(x+6)
 # also can't go higher than x**2 or do equations with coefficients for x**2
 
@@ -7,7 +7,8 @@ coeX = int(input('What is the coefficient for power x?'))
 coe1 = int(input('What is the coefficient for power 1?'))
 
 # coe1 is the higher power, coe2 is the lower power
-
+zero1 = 'x'
+zero2 = 'y'
 
 # finds the zeros of the equation
 for i in range(-50, 50):
@@ -21,7 +22,11 @@ for i in range(zero1+1, 50):
 
 
 def answer():
-    print('(x + ' + str(zero1) + ')(x + ' + str(zero2) + ')')
+    # Prints answer based on zeros.
+    if zero2 != 'y':
+        print('(x + ' + str(-zero1) + ')(x + ' + str(-zero2) + ')')
+    else:
+        print('(x + '+ str(-zero1) + ') ** 2')
 
 
 answer()
